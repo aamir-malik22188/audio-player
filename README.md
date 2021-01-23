3de25ebf47ab2c3a772384dc910d92738b7da231
Aamir Malik <aamir22188@gmail.com>
2021-01-23 15:13:25 -0600

    initial project load

---
+++ README.md
@@ -0,0 +1,111 @@
+<!-- Title -->
+
+<br />
+  <h1 align="center">Audio Player</h1>
+
+  <p align="center">
+    An Audio Player Project
+    <br />
+    <br />
+    <a href="">View Demo</a>
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgements">Acknowledgements</a></li>
+  </ol>
+</details>
+<br/>
+<br/>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<img src="images/giphy-landing(readme).png" height="400" width="700">
+<br>
+<img src="images/search-gifs-section(readme).png" height="400" width="700">
+<br/>
+<br/>
+
+
+As a fan of music, I wanted to build an app that would utilize HTML, CSS, and Javascript.
+
+Objectives:
+* 
+* 
+* 
+
+
+
+
+### Built With
+
+The following frameworks were used to complete the project.
+* [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+* [MaterializeCSS]()
+* [Javascript](https://www.javascript.com/)
+
+
+
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+The project can either be viewed at this link [Audio Player](www.heroku.com/) or the code can be cloned onto a local machine with the steps below.
+
+
+### Installation
+
+1. Clone the repository:
+   ```sh
+   git clone 
+   ```
+
+2. Start project.
+
+
+
+
+<!-- CONTACT -->
+## Contact
+
+
+Aamir Malik - Aamir22188@gmail.com
+
+Project Repo: 
+
+Twitter Handle: [@TheRealAamir](https://twitter.com/TheRealAamir)
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+* [Heroku](www.heroku.com/)
+* [W3 Schools](https://www.w3schools.com/)
+* [Audio Library](https://www.audiolibrary.com.co/)
+* [SMU Bootcamp](https://techbootcamps.smu.edu/)
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,207 @@
+let nowPlaying = document.querySelector("#nowPlaying"); 
+let albumImage = document.querySelector("#albumImage"); 
+let songName = document.querySelector("#songName"); 
+let artistName = document.querySelector("#artistName"); 
+  
+let playBtn = document.querySelector("#play"); 
+let skipNextBtn = document.querySelector("#skip-next"); 
+let skipPreviousBtn = document.querySelector("#skip-previous"); 
+  
+let songSlider = document.querySelector("#songSlide"); 
+let currentTime = document.querySelector(".currentTime"); 
+let totalDuration = document.querySelector(".totalDuration"); 
+  
+// Specify globally used values 
+let trackIndex = 0; 
+let isPlaying = false; 
+let updateTimer; 
+  
+// Create the audio element for the player 
+let currentTrack = document.createElement('audio'); 
+  
+// Define the list of tracks that have to be played 
+let trackList = [ 
+  { 
+    name: "Bad Intentions", 
+    artist: "Le Gang", 
+    image: "/lofi-player/images/bad-intentions.png", 
+    path: "/lofi-player/audio/bad-intentions.mp3",
+  }, 
+  { 
+    name: "Come Along", 
+    artist: "Jay Someday", 
+    image: "/lofi-player/images/come-along.png", 
+    path: "/lofi-player/audio/come-along.mp3"
+  }, 
+  { 
+    name: "Echoes", 
+    artist: "LiQWYD", 
+    image: "/lofi-player/images/echoes.png", 
+    path: "/lofi-player/audio/echoes.mp3", 
+  }, 
+  { 
+    name: "Voice", 
+    artist: "KV", 
+    image: "/lofi-player/images/voice.jpg", 
+    path: "/lofi-player/audio/voice.mp3", 
+  }, 
+  { 
+    name: "Coast", 
+    artist: "Your Friend, Ghost", 
+    image: "/lofi-player/images/coast.png", 
+    path: "/lofi-player/audio/coast.mp3", 
+  }, 
+]; 
+
+function loadTrack(trackIndex) { 
+  // Clear the previous seek timer 
+  clearInterval(updateTimer); 
+  resetValues(); 
+  
+  // Load a new track 
+  currentTrack.src = trackList[trackIndex].path; 
+  currentTrack.play(); 
+  
+  // Update details of the track 
+  albumImage.style.backgroundImage = "url(" + trackList[trackIndex].image + ")";
+  // original albumImage.backgroundimage = "url("  + trackList[trackIndex].image + ")"; 
+  songName.textContent = trackList[trackIndex].name; 
+  artistName.textContent = trackList[trackIndex].artist; 
+  nowPlaying.textContent =   "PLAYING " + (trackIndex + 1) + " OF " + trackList.length; 
+  
+  // Set an interval of 1000 milliseconds 
+  // for updating the seek slider 
+  updateTimer = setInterval(seekUpdate, 1000); 
+  
+  // Move to the next track if the current finishes playing 
+  // using the 'ended' event 
+  currentTrack.addEventListener("ended", nextTrack); 
+  
+  //Apply a random background color 
+  random_bg_color(); 
+} 
+  
+
+  
+// Functiom to reset all values to their default 
+function resetValues() { 
+  currentTime.textContent = " : "; 
+  totalDuration.textContent = "00:00 "; 
+  songSlider.value = 0; 
+} 
+
+
+function playpauseTrack() { 
+  // Switch between playing and pausing 
+  // depending on the current state 
+  if (!isPlaying) playTrack(); 
+  else pauseTrack(); 
+} 
+  
+function playTrack() { 
+  // Play the loaded track 
+  currentTrack.play(); 
+  isPlaying = true; 
+  
+  // Replace icon with the pause icon 
+  playBtn.innerHTML = '<i class="small material-icons">pause</i>'; 
+} 
+  
+function pauseTrack() { 
+  // Pause the loaded track 
+  currentTrack.pause(); 
+  isPlaying = false; 
+  
+  // Replace icon with the play icon 
+  playBtn.innerHTML = '<i class="small material-icons">play_arrow</i>';
+} 
+  
+function nextTrack() { 
+  // Go back to the first track if the 
+  // current one is the last in the track list 
+  if (trackIndex < trackList.length - 1) 
+    trackIndex += 1; 
+  else trackIndex = 0; 
+  
+  // Load and play the new track 
+  loadTrack(trackIndex); 
+  playTrack(); 
+} 
+  
+function prevTrack() { 
+  // Go back to the last track if the 
+  // current one is the first in the track list 
+  if (trackIndex > 0) 
+    trackIndex -= 1; 
+  else trackIndex = trackList.length; 
+    
+  // Load and play the new track 
+  loadTrack(trackIndex); 
+  playTrack(); 
+} 
+
+
+function seekTo() { 
+  // Calculate the seek position by the 
+  // percentage of the seek slider  
+  // and get the relative duration to the track 
+  seekto = currentTrack.duration * (songSlider.value / 100); 
+  
+  // Set the current track position to the calculated seek position 
+  currentTrack.currentTime = seekto; 
+} 
+  
+// function setVolume() { 
+//   // Set the volume according to the 
+//   // percentage of the volume slider set 
+//   currentTrack.volume = volume_slider.value / 100; 
+// } 
+  
+function seekUpdate() { 
+  let seekPosition = 0; 
+  
+  // Check if the current track duration is a legible number 
+  if (!isNaN(currentTrack.duration)) { 
+    seekPosition = currentTrack.currentTime * (100 / currentTrack.duration); 
+    songSlider.value = seekPosition; 
+  
+    // Calculate the time left and the total duration 
+    let currentMinutes = Math.floor(currentTrack.currentTime / 60); 
+    let currentSeconds = Math.floor(currentTrack.currentTime - currentMinutes * 60); 
+    let durationMinutes = Math.floor(currentTrack.duration / 60); 
+    let durationSeconds = Math.floor(currentTrack.duration - durationMinutes * 60); 
+  
+    // Add a zero to the single digit time values 
+    if (currentSeconds < 10) { currentSeconds = " " + currentSeconds; } 
+    if (durationSeconds < 10) { durationSeconds = " " + durationSeconds; } 
+    if (currentMinutes < 10) { currentMinutes = " " + currentMinutes; } 
+    if (durationMinutes < 10) { durationMinutes = " " + durationMinutes; } 
+  
+    // Display the updated duration 
+    currentTime.textContent = currentMinutes + " : " + currentSeconds; 
+    totalDuration.textContent = durationMinutes + " : " + durationSeconds; 
+  } 
+
+}
+
+function random_bg_color() { 
+  // Get a random number between 64 to 256 
+  // (for getting lighter colors) 
+  let red = Math.floor(Math.random() * 256) + 64; 
+  let green = Math.floor(Math.random() * 256) + 64; 
+  let blue = Math.floor(Math.random() * 256) + 64; 
+  
+  // Construct a color withe the given values 
+  let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")"; 
+  
+  // Set the background to the new color 
+  document.getElementById('consoleSection').style.background = bgColor; 
+} 
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
+
